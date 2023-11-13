7a31dc81d1ca0d4f8aafaea3c2fc6e84e44ff14b
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-13 19:15:30 +0500

    2task

---
+++ 2task.py
@@ -0,0 +1,9 @@
+a = ["dushanba","seshanba","chorshanba","payshanba","juma","shanba","yakshanba"]
+
+
+b = ['yanvar','fevral','mart','aprel','may','iyun','iyul','avgust','sentabr','oktabr','noyabr','dekabr']
+
+
+c = a+b
+
+print(c)
